96b0e003510f9175bd0215d906adca9e860bb0d4
CodingCerrado <115609215+CodingCerrado@users.noreply.github.com>
2022-10-12 19:10:03 +1000

    fix array issu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const arrayResposta = ['GIT', "GITHUB", "JAVASCRIPT", "CSS", "HTML", "TRELLO", "SLACK"]
 function onClick() {
-    document.getElementById("text").innerHTML = nosSomos[Math.floor(Math.random() * arrayResposta.length)];
+    document.getElementById("text").innerHTML = arrayResposta[Math.floor(Math.random() * arrayResposta.length)];
 }
