18dbdb792fed747006ccd23129b631547d88d01e
CodingCerrado <115609215+CodingCerrado@users.noreply.github.com>
2022-10-12 19:07:10 +1000

    change array values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nosSomos = ['GIT', "GITHUB", "JAVASCRIPT", "CSS", "HTML", "TRELLO", "SLACK"]
+const arrayResposta = ['GIT', "GITHUB", "JAVASCRIPT", "CSS", "HTML", "TRELLO", "SLACK"]
 function onClick() {
-    document.getElementById("text").innerHTML = nosSomos[Math.floor(Math.random() * nosSomos.length)];
+    document.getElementById("text").innerHTML = nosSomos[Math.floor(Math.random() * arrayResposta.length)];
 }
